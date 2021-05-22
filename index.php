f2999947c06acfe124cc217550d7c1b5a2c7346b
Bryan Francia <bri.francia@gmail.com>
2021-05-22 16:35:58 -0300

    formulario hecho

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escrito 3</title>
+</head>
+<body>
+    
+    <form action="" method="post">
+    
+    <form id="formulario" action="/insertar.php" method="post">
+        Nombre: <input type="text" name="nombre" id="nombre"> <br>
+        Apellido: <input type="text" name="apellido" id="apellido"> <br>
+        Telefono: <input type="number" name="telefono" id="telefono"> <br>
+        Fecha de nacimiento: <input type="date" name="date" id="date"> <br>
+        Edad: <input type="number" name="edad" id="edad"> <br>
+        Email: <input type="email" name="email" id="email"> <br>
+        <button>Enviar</button>
+    </form>
+    
+    
+    
+    
+    
+    
+    
+    
+    </form>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
