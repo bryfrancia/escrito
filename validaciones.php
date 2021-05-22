d2e15e6654cac55a17cefcafcd319349096e9035
Bryan Francia <bri.francia@gmail.com>
2021-05-22 18:25:32 -0300

    validacion de campos completa

--- validaciones.php
+++ validaciones.php
@@ -1,7 +1,9 @@
 <?php  validacionCampos();
 
         function validacionCampos(){
-            if($_POST['nombre'] === "") {
+            if($_POST['nombre'] === "" || $_POST['apellido'] === "" || $_POST['telefono'] === "" || $_POST['fechaNacimiento'] === "" ||
+            $_POST['edad'] === "" || $_POST['email'] === ""
+            ) {
                 echo "Complete todos los campos";
                
                 }
